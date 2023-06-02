77e95b3c447873f6efe71b60c6b9681b3fe3a644
sofiamachado <sofia25.machado@gmail.com>
2023-06-02 20:16:49 -0300

    bootstrap

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -1,27 +1,35 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<scr ipt src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+ 
 <html>
     <body>
-          <div class="text-right">
-              <a href="{{url('addDados')}}" class="text-info">Crie uma linha na tabela</a>
-              <!-- <a> Casa mais cara </a> -->
-              <a> Aluguel </a>
-              <a> Compra </a>
-              <a> Ordem crescente - Endereço </a>
-              <a> Ordem crescente - Preço </a>
-              <a> <input type="text" placeholder="Pesquisa">  </a>
-          </div>
+      <br />
+      <div class="container">
+          <div id="login-row" class="justify-content-md-between">
+            <a href="{{url('addDados')}}" class="text-info form-control"> ➕ Crie uma linha na tabela</a>
+            <!-- <a> Casa mais cara </a> -->
+            <a class="text-info form-control"> Aluguel ou Compra </a>
+            <a class="text-info form-control"> 👆🏻 Ordem crescente - Endereço </a>
+            <a class="text-info form-control"> 👆🏻 Ordem crescente - Preço </a>
+            <a class="text-info form-control"> 🔎 <input type="text" placeholder="Pesquisa">  </a>
+          </div>  
         @forelse ($selecoes as $sel)
-          <hr class="solid">
-          <li> Imobiliaria: {{ $sel->Imobiliaria }}</li>
-          <li> Endereço: {{ $sel->Endereco }}</li>
-          <li> Preço: {{$sel->Preco }} </li>
-          <li> Status: {{$sel->Status }} </li>
-          <div class="text-right">
-            <a href="{{url('editaDados/'.$sel['id'])}}"> Edita </a>
-            <a href="{{url('deletaDados/'.$sel['id'])}}" class="text-info" onclick="return confirm('Deletar casa?')">Deleta</a>
+          <div>
+            <hr class="solid">
+            <li> Imobiliaria: {{ $sel->imobiliaria }}</li>
+            <li> Endereço: {{ $sel->endereco }}</li>
+            <li> Preço: {{$sel->preco }} </li>
+            <li> Status: {{$sel->status }} </li>
+            <div class="text-right">
+              <a href="{{url('editaDados/'.$sel['id'])}}" class=" text-white bg-primary p-2 mr-1"> Edita </a>
+              <a href="{{url('deletaDados/'.$sel['id'])}}" class="text-white bg-danger p-2" onclick="return confirm('Deletar casa?')">Deleta</a>
+            </div>
           </div>
         @empty
           <p> Está vazio </p>
+      </div>
         @endforelse
     </body>
 </html>
